c0678a747696c29a0e36c994a065ee68944b49e6
Awang Tirta Ananta <90815489+Comethruuu@users.noreply.github.com>
2025-05-24 15:16:42 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,12 @@
 fx_version 'cerulean'
 games { 'gta5' }
 author 'cloud'
+repository 'https://github.com/Comethruuu/cloud-editor.git'
 lua54 'yes'
+
 client_scripts {
 'client/*'
 }
-
 
 shared_scripts {
     '@ox_lib/init.lua',
